0c8bb1aab376b8048f8e4da644677b61b67d2515
dzimentey <dzimentey@gmail.com>
2021-10-01 01:03:12 +0300

    add: login unlogin display

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 import logo from "../../logo.png";
-import React from "react";
+import React, {ReactNode} from "react";
 import c from './Header.module.css'
 import { NavLink } from "react-router-dom";
 
-export const Header = () => {
+
+export const Header = (props: any) => {
+
     return (
         <header className={c.header}>
             <img src={logo} className={c.AppLogo} alt="logo"/>
             <div className={c.loginBlock}>
-                <NavLink to={'/login'}>Login</NavLink>
+                { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
             </div>
         </header>
     )
