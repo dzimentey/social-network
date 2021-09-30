f2d5b15b7c091fb02060af5956641c62e771d33b
dzimentey <dzimentey@gmail.com>
2021-09-30 23:27:20 +0300

    add: loginBlock style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import logo from "../../logo.png";
 import React from "react";
 import c from './Header.module.css'
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
     return (
         <header className={c.header}>
             <img src={logo} className={c.AppLogo} alt="logo"/>
+            <div className={c.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
         </header>
     )
 }
