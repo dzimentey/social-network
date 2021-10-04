265d457d8e9d0ac9edd0277d601e3d80a3d4550e
dzimentey <dzimentey@gmail.com>
2021-10-04 20:50:13 +0300

    add: instance by axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,50 +2,39 @@ import axios from "axios";
 
 const baseUrl: string = `https://social-network.samuraijs.com/api/1.0/`
 
-export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
-
-    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
+const instance = axios.create({
         withCredentials: true,
+        baseURL: baseUrl,
         headers: {
             "API-KEY": "49c9fc27-b65d-436b-ad55-f34f2b452a65"
-        }
-    })
+        },
+    }
+)
+
+export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
+
+    return instance.get( `users?page=${currentPage}&count=${pageSize}`)
         .then(response => {
             return response.data
         })
 }
 
 export const getUserData = (userId: number) => {
-    return axios.get(baseUrl + `profile/${userId}`, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "49c9fc27-b65d-436b-ad55-f34f2b452a65"
-        }
-    })
+    return instance.get( `profile/${userId}`, )
         .then(response => {
             return response.data
         })
 }
 
 export const unFollowUser = (id: string | number) => {
-    return axios.delete(baseUrl + `follow/${id}`,{
-        withCredentials: true,
-        headers: {
-            "API-KEY" : "49c9fc27-b65d-436b-ad55-f34f2b452a65"
-        }
-    })
+    return instance.delete(baseUrl + `follow/${id}`, )
         .then(response => {
             return response.data
         })
 }
 
 export const followUser = (id: string | number) => {
-    return axios.post(baseUrl + `follow/${id}`, {}, {
-        withCredentials: true,
-        headers: {
-            "API-KEY" : "49c9fc27-b65d-436b-ad55-f34f2b452a65"
-        }
-    })
+    return instance.post(baseUrl + `follow/${id}`, {}, )
         .then(response => {
             return response.data
         })
