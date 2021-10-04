465500ae1657704db4be1089ba3e3b5f790b1098
dzimentey <dzimentey@gmail.com>
2021-10-04 19:24:18 +0300

    add: getUserData() - request to server

--- src/api/api.ts
+++ src/api/api.ts
@@ -12,3 +12,15 @@ export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
             return response.data
         })
 }
+
+export const getUserData = (userId: number) => {
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`, {
+        withCredentials: true,
+        headers: {
+            "API-KEY": "49c9fc27-b65d-436b-ad55-f34f2b452a65"
+        }
+    })
+        .then(response => {
+            return response.data
+        })
+}
