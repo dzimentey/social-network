756aef0d9a15f1e978cac7b5eaea385863014857
dzimentey <dzimentey@gmail.com>
2021-10-04 20:02:30 +0300

    add: followUser() server request

--- src/api/api.ts
+++ src/api/api.ts
@@ -36,3 +36,15 @@ export const unFollowUser = (id: string | number) => {
             return response.data
         })
 }
+
+export const followUser = (id: string | number) => {
+    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
+        withCredentials: true,
+        headers: {
+            "API-KEY" : "49c9fc27-b65d-436b-ad55-f34f2b452a65"
+        }
+    })
+        .then(response => {
+            return response.data
+        })
+}
