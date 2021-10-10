a3f61e970698b6eca9dfe5989ecd75b14378976d
dzimentey <dzimentey@gmail.com>
2021-10-10 12:53:49 +0300

    refactor: change setAuthUserData to getAuthUserData

--- src/components/Header/headerContainer.tsx
+++ src/components/Header/headerContainer.tsx
@@ -2,29 +2,27 @@ import axios from "axios";
 import React from "react";
 import {Header} from "./Header";
 import {connect} from "react-redux";
-import {setAuthUserData} from "../../Redux/auth-reducer";
+import {getAuthUserData, setAuthUserData} from "../../Redux/auth-reducer";
 import {AppStateType} from "../../Redux/redux-store";
+import {authAPI} from "../../api/api";
 
 
 type HeaderContainerComponentType = {
-    setAuthUserData: (id: number, email: string, login: string) => void
+    //setAuthUserData: (id: number, email: string, login: string) => void
+    getAuthUserData: () => void
 }
 
  class HeaderContainerComponent extends React.Component<HeaderContainerComponentType, any> {
     componentDidMount() {
 
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-            withCredentials: true,
-            headers: {
-                "API-KEY": "49c9fc27-b65d-436b-ad55-f34f2b452a65"
-            }
-        })
-            .then((response) => {
-                if (response.data.resultCode === 0) {
-                    let {id, email, login} = response.data.data;
-                    this.props.setAuthUserData(id, email, login);
-                }
-            });
+        // axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+        //     withCredentials: true,
+        //     headers: {
+        //         "API-KEY": "49c9fc27-b65d-436b-ad55-f34f2b452a65"
+        //     }
+        // })
+
+       this.props.getAuthUserData()
     }
     render() {
         return <Header {...this.props}/>;
@@ -35,4 +33,4 @@ const mapStateToProps = (state: AppStateType) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
 });
- export const HeaderContainer = connect(mapStateToProps, {setAuthUserData})(HeaderContainerComponent)
+ export const HeaderContainer = connect(mapStateToProps, {getAuthUserData: getAuthUserData})(HeaderContainerComponent)
