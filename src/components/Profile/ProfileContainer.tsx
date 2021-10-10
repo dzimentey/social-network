57c73009e48b93517b5b8879f4a3c8bf54ae8f74
dzimentey <dzimentey@gmail.com>
2021-10-10 12:55:17 +0300

    refactor: change setUserProfile to getUserProfile

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,41 +1,41 @@
 import React from "react";
-import {stateType, StoreType} from "../../Redux/store";
+
 import {Profile} from "./Profile";
-import axios from "axios";
+
 import {connect} from "react-redux";
-import {setUserProfile} from "../../Redux/profileReducer";
+import {getUserProfile, setUserProfile} from "../../Redux/profileReducer";
 import {AppStateType} from "../../Redux/redux-store";
-import {RouteComponentProps, withRouter } from "react-router";
-import {getUserData} from "../../api/api";
+import {RouteComponentProps, withRouter} from "react-router";
+//import {getUserData} from "../../api/api";
 
 type ProfileContainerPropsType = {
-   // store: AppStateType//StoreType
-    setUserProfile: (profile: any) => void
+    // store: AppStateType//StoreType
+   //setUserProfile: (profile: any) => void
     profile: any
+    getUserProfile: (userId: number) => void
 }
 
 type PathParamType = {
     userId: string | any
+
 }
 
 type CommonPropsType = RouteComponentProps<PathParamType> & ProfileContainerPropsType
 
- class ProfileContainerComponent extends React.Component<CommonPropsType, any> {
+class ProfileContainerComponent extends React.Component<CommonPropsType, any> {
 
     componentDidMount() {
         let userId = this.props.match.params.userId
-        if(!userId){userId = 2 }
+        if (!userId) {
+            userId = 2
+        }
         // axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`, {
         //     withCredentials: true,
         //     headers: {
         //         "API-KEY" : "49c9fc27-b65d-436b-ad55-f34f2b452a65"
         //     }
         // })
-        getUserData(userId)
-            .then((data) => {
-                this.props.setUserProfile(data );
-
-            });
+        this.props.getUserProfile(userId)
 
     }
 
@@ -52,5 +52,5 @@ const mapStateToProps = (state: AppStateType) => ({
 
 let WithUrlDataContainerComponent = withRouter(ProfileContainerComponent)
 
-export const ProfileContainer = connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent)
+export const ProfileContainer = connect(mapStateToProps, {getUserProfile})(WithUrlDataContainerComponent)
 
