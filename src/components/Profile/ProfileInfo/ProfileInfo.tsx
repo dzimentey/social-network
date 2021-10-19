56838d1222645219f5e1e938bef7a4bdf16963cc
dzimentey <dzimentey@gmail.com>
2021-10-19 16:20:34 +0300

    refactor: remove main ww1 image

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import {Preloader} from "../../coomon/preloader/Preloader";
 import userPhoto from "../../../assets/images/homer-warrior.jpg";
+import {ProfileStatus} from "./ProfileStatus";
 
 type ProfileInfoType = {
     profile: any
 }
 export const ProfileInfo = (props: ProfileInfoType) => {
-    if(!props.profile){
+    if (!props.profile) {
         return <Preloader/>
     }
 
     return (
         <div>
-            <div>
-                <img alt={'warriors'} width={'100%'} height={'350 px'}
-                     src={'https://petapixel.com/assets/uploads/2016/10/earnestbrooks.jpg'}/>
-            </div>
-            <div>
+            {/*<div>*/}
+            {/*    <img alt={'warriors'} width={'100%'} height={'350 px'}*/}
+            {/*         src={'https://petapixel.com/assets/uploads/2016/10/earnestbrooks.jpg'}/>*/}
+            {/*</div>*/}
 
+            <div>
                 <img src={props.profile.photos.large ? props.profile.photos.large : userPhoto}/>
+                <ProfileStatus/>
             </div>
+
         </div>
     )
 }
+
