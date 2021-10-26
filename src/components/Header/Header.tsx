050fca4e7982d6c34a4e54d5dc32ee39a0d038da
dzimentey <dzimentey@gmail.com>
2021-10-26 18:37:33 +0300

    add: Log out Button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import logo from "../../logo.png";
-import React, {ReactNode} from "react";
+import React from "react";
 import c from './Header.module.css'
 import { NavLink } from "react-router-dom";
 
@@ -10,7 +10,9 @@ export const Header = (props: any) => {
         <header className={c.header}>
             <img src={logo} className={c.AppLogo} alt="logo"/>
             <div className={c.loginBlock}>
-                { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+                { props.isAuth
+                    ? <div>{props.login}  <button onClick={props.logout}>Log out</button></div>
+                    : <NavLink to={'/login'}>Login</NavLink> }
             </div>
         </header>
     )
