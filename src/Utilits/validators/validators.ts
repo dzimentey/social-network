0573c89e57ed0ba41c0a12c93f5dfee94fbc8ecd
dzimentey <dzimentey@gmail.com>
2021-10-26 15:04:25 +0300

    add: Max Length validation creator

--- src/Utilits/validators/validators.ts
+++ src/Utilits/validators/validators.ts
@@ -1,7 +1,19 @@
 
-export const requiredFields = (value: string) => {
+export const requiredField = (value: string) => {
     if(value) return undefined;
 
     return 'Field is required';
 
 }
+
+export const maxLengthCreator = (maxLength: number) => (value: string) => {
+    if(value && value.length > maxLength) return `Max length should be fewer or equal ${maxLength} characters `;
+
+    return undefined ;
+}
+
+// export const maxLength30 = (value: string) => {
+//     if(value && value.length > 30) return 'Max length should be fewer or equal 30 characters ';
+//
+//     return undefined ;
+// }
