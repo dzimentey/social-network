ecbd91b9043552d3067e54e5b470da3abd7cb5b3
dzimentey <dzimentey@gmail.com>
2021-11-01 20:09:33 +0300

    refactor: move authorisation to App

--- src/components/Header/headerContainer.tsx
+++ src/components/Header/headerContainer.tsx
@@ -1,24 +1,19 @@
-import axios from "axios";
+
 import React from "react";
 import {Header} from "./Header";
 import {connect} from "react-redux";
-import {getAuthUserData, logout, setAuthUserData} from "../../Redux/auth-reducer";
+import {logout} from "../../Redux/auth-reducer";
 import {AppStateType} from "../../Redux/redux-store";
-import {authAPI} from "../../api/api";
+
 
 
 type HeaderContainerComponentType = {
     //setAuthUserData: (id: number, email: string, login: string) => void
-    getAuthUserData: () => void
+    //getAuthUserData: () => void
 }
 
  class HeaderContainerComponent extends React.Component<HeaderContainerComponentType, any> {
-    componentDidMount() {
 
-
-
-       this.props.getAuthUserData()
-    }
     render() {
         return <Header {...this.props}/>;
     }
@@ -28,4 +23,4 @@ const mapStateToProps = (state: AppStateType) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
 });
- export const HeaderContainer = connect(mapStateToProps, {getAuthUserData: getAuthUserData, logout})(HeaderContainerComponent)
+ export const HeaderContainer = connect(mapStateToProps, { logout})(HeaderContainerComponent)
